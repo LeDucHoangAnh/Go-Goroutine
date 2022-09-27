b91b6210613763b4493678b0220aa351b7ee1d35
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-27 15:28:27 +0700

    Synchronized goroutines

--- go-routine.go
+++ go-routine.go
@@ -2,20 +2,48 @@ package main
 
 import (
 	"fmt"
-	"runtime"
-	"time"
+	"sync"
 )
 
 func g1() {
 	fmt.Println("g1")
+	wg.Done()
 }
+
+func g2() {
+	fmt.Println("g2")
+	wg.Done()
+
+}
+
+var wg sync.WaitGroup
 
 func main() {
 	//Khai báo 1 go routine: go func()
+	// go g1()
+
+	// ng := runtime.NumGoroutine()
+	// fmt.Println(ng)
+
+	// time.Sleep(time.Second)
+
+	//Synchronized goroutines
+	/*
+		logic 1
+
+		go g1()
+		go g2()
+
+		logic 2
+
+	*/
+	fmt.Println("Begin")
+	wg.Add(2)
+
 	go g1()
+	go g2()
 
-	ng := runtime.NumGoroutine()
-	fmt.Println(ng)
+	wg.Wait()
+	fmt.Println("End")
 
-	time.Sleep(time.Second)
 }
